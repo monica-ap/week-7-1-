d0d9f9fa45af984ec321f22d4583197d4974462e
Monica Pena <113930652+monica-ap@users.noreply.github.com>
2022-10-04 17:27:34 +0000

    learned about loops and how to use them.

---
+++ loops.py
@@ -0,0 +1,70 @@
+def loop():
+  names=["mary", "john", "david", "thomas", "janette"]
+  #for name in names:
+   # print(f"hello {name}")
+  ###################################loops intro######################################
+  # queue videos
+  #what is iteration?
+  #what are for loops?
+  
+#a = 1
+#while a < 10:
+#  print(a)
+#  a=a+1
+
+
+
+#  num_list = [1,2,3,4,5,6,7,8,9,10]
+
+  #for num in num_list:
+  #  sum=0
+    #sum= sum+num
+   # print(sum)
+
+
+names=["mary", "john", "david", "thomas", "janette"]
+
+#for name in names:
+  #if name.startswith("j"):
+   # print(f"this {name} starts with j")
+
+#dictionaries
+dic= {"key1": "a", "key2": "b", "key3": "c"}
+for item in dic:
+  print(item)
+  # For Loops Practice #1
+  # Using For loops, greet all members of a class, printing "Hello" + their name.
+  
+  # For example: "Hello Norville"
+  
+  students = ["Norville", "Fred", "Velma", "Daphne"]
+  
+  
+  
+  
+  # For Loops Practice #2
+  # Given the following list of numbers, calculate the sum of all the numbers using For loops and store the result of the sum in a variable called sum_numbers:
+  
+  list_numbers = [1,5,8,7,6,8,2,5,2,6,4,8,5,9,8,3,5,4,2,5,6,4]
+  # sum_numbers = 
+  
+  
+  
+  
+  
+  # For Loops Practice #3
+  # Given the following list of numbers, perform the sum of all even and odd* numbers separately in the variables sum_even and sum_odd respectively:
+  
+  list_numbers = [1,5,8,7,6,8,2,5,2,6,4,8,5,9,8,3,5,4,2,5,6,4]
+  
+  # *Recall from previous days: the modulus (or remainder) of a number divided by 2 is zero when said value is even, and 1 when it is odd
+  
+  # num % 2 == 0 (even values)
+  
+  # num % 2 == 1 (odd values)
+  
+  list_numbers = [1,5,8,7,6,8,2,5,2,6,4,8,5,9,8,3,5,4,2,5,6,4]
+  
+  # sum_even = 
+  
+  # sum_odd = 
